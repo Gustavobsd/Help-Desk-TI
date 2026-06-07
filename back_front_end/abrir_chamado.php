7a900b71815a8b4f9cbd4a1e08d58af22783d7b6
Gustavo Bruno <gustavobsd24@gmail.com>
2026-06-07 10:11:45 -0300

    Organiza repositório para entrega acadêmica: mantém raiz e remove duplicatas

--- back_front_end/abrir_chamado.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (empty($_SESSION['usuario_id'])) {
-    header('Location: login.php');
-    exit;
-}
-header('Location: dashboard.php');
-exit;
--- data-base/teste_conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include_once('config.php');
-
-echo "Banco conectado com sucesso!";
-
-?>
